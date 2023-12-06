36102ca0db7084bb8ecd0a24ba4e28e8618c4de4
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2023-12-06 15:58:34 +0530

    completed part1 of day03

---
+++ day03/go.mod
@@ -0,0 +1,3 @@
+module github.com/Mohd-Sayeedul-Hoda/day03
+
+go 1.20
---
+++ day03/part1.go
@@ -0,0 +1,64 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func main(){
+	f, err := os.Open(os.Args[1])
+	if err != nil{
+		fmt.Fprintf(os.Stderr, "Can't open the file you provide\n")
+	}
+	fmt.Println(readFile(f))
+}
+
+func readFile(f *os.File)int{
+	input := bufio.NewScanner(f)
+	ans := 0
+	for input.Scan(){
+		str := input.Text()
+		ans += commonItem(str)
+	}
+	return ans
+}
+
+func commonItem(str string)int{
+	mid := int(len(str)/2)
+	firstRack := str[:mid]
+	secondRack := str[mid:]
+	itemMap := make(map[byte]int8)
+	repeatedItem := make([]int, 0)
+	
+	for _, v := range firstRack{
+		c := byte(v)
+		if _, exists := itemMap[c]; exists {
+			continue
+		}else{
+			itemMap[c] = 1
+		}
+	}
+	for _, v := range secondRack{
+		c := byte(v)
+		if _, exists := itemMap[c]; exists && itemMap[c] <= 1 {
+			repeatedItem = append(repeatedItem, int(c))
+			itemMap[c]++
+		}else{
+			continue
+		}
+	}
+	return  getPiority(&repeatedItem)
+}
+
+func getPiority(item *[]int)int{
+	val := 0
+	for _, v := range *item{
+		if v >= 97{
+			val += (v - 96)
+		}else{
+			val += (v - 38)	
+		}
+	}
+	return val
+}
