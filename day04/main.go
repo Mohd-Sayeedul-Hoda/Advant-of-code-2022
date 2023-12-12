f59df81e4d002eb56d04106de4c8c1badc31ca1b
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2023-12-12 12:32:54 +0530

    completed part1 should complete part2 next

--- day04/main.go
+++ day04/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,24 +14,46 @@ func main(){
 		fmt.Fprintf(os.Stderr, "Can't open the file")
 		return
 	}
+
+	defer file.Close()
+
 	input := bufio.NewScanner(file)
 	ans := 0
+	i := 1 
 	for input.Scan(){
 		str := input.Text()
 		strArr := strings.Split(str, ",")
-		ans += checkOverlap(strArr)
+		ans += checkOverlap(strArr, i)
+		i++
 	}
 	fmt.Println(ans)
 }
 
-func checkOverlap(strArr []string)int{
+func checkOverlap(strArr []string, line int)int{
 	str1 := strings.Split(strArr[0], "-")
 	str2 := strings.Split(strArr[1], "-")
-	if str1[0] >= str2[0] && str1[1] <= str2[1]{
+	int1 := stringToIntArray(str1)
+	int2 := stringToIntArray(str2)
+	if int1[0] >= int2[0] && int1[1] <= int2[1]{ 
+		fmt.Println(line)
+		fmt.Println(str1, str2)
 		return 1
-	}else if str2[0] >= str1[0] && str2[1] <= str1[1]{
+	}else if int2[0] >= int1[0] && int2[1] <= int1[1]{
+		fmt.Println("condition 2")
+		fmt.Println(line)
+		fmt.Println(str1, str2)
 		return 1
 	}
 	return 0
 }
-
+func stringToIntArray(str []string) ([]int) {
+	var intArr []int
+	for _, s := range str{
+		val, err := strconv.Atoi(s)
+		if err != nil{
+			fmt.Fprintf(os.Stderr, "cannot convert to int")
+		}
+		intArr = append(intArr, val)
+	}
+	return intArr
+}
