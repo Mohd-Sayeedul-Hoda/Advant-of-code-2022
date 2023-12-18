ec2ac001b0ea274ebb2a9e57cbcdfa459f6b086d
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2023-12-18 21:40:41 +0530

    I think half done

--- day05/part1/part1.go
+++ day05/part1/part1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Stack struct{  
@@ -19,19 +20,26 @@ func (s *Stack) Push(char byte){
 }
 
 func (s *Stack) Peek() byte{
-	return s.slice[0]
+	return s.slice[len(s.slice)-1]
 }
 
 func (s *Stack)Pop()byte{
 	var temp byte = s.Peek()
-	s.slice = s.slice[1:len(s.slice)]
+	s.slice = s.slice[0:len(s.slice)-1]
 	return temp
 }
 
 func Part1(file *os.File){
 	input := bufio.NewScanner(file)
-	arr := MakeStack(file, input)
-	fmt.Println(len(arr))
+	arr := MakeStack(input)
+	for _, v := range arr{
+		for _, val := range v.slice{
+			fmt.Printf("%c", val)
+		}
+		fmt.Printf("\n")
+	}
+	Move(input, arr)
+	fmt.Printf("\n")
 	for _, v := range arr{
 		for _, val := range v.slice{
 			fmt.Printf("%c", val)
@@ -40,7 +48,7 @@ func Part1(file *os.File){
 	}
 }
 
-func MakeStack(file *os.File,input *bufio.Scanner)[]*Stack{
+func MakeStack(input *bufio.Scanner)[]*Stack{
 	var arr []*Stack
 	firstTime := true
 	for input.Scan(){
@@ -54,16 +62,14 @@ func MakeStack(file *os.File,input *bufio.Scanner)[]*Stack{
 				arr = append(arr, x)
 			}
 		}
-		fmt.Println(str)
 		if string(str[1]) == "1"{
 			return arr
 		}
 		s := 0 // for stack value
-		for i := 0; i < len(str);  {
+		for i := 0; i < len(str); i += 4  {
 			if string(str[i+1]) != " "{
 				arr[s].PushLast(str[i+1])
 			}	
-			i +=4
 			s +=1
 		}
 	} 
@@ -71,3 +77,31 @@ func MakeStack(file *os.File,input *bufio.Scanner)[]*Stack{
 	return arr
 }
 
+func Move(input *bufio.Scanner, arr []*Stack){
+//	firstTime := true
+	for input.Scan(){
+		str := input.Text()
+		if string(str) == ""{
+			continue
+		}
+		howMuch, err := strconv.Atoi(string(str[5]))
+		if err != nil{
+			fmt.Println("cannot convert it into string")
+			return
+		}
+		from, err := strconv.Atoi(string(str[12]))
+		if err != nil{
+			fmt.Println("cannot convert it into string")
+			return
+		}
+		to, err := strconv.Atoi(string(str[17]))
+		if err != nil{
+			fmt.Println("cannot convert it into string")
+			return
+		}
+		for i := 0; i < howMuch;  i++{
+			temp := arr[from-1].Pop()	
+			arr[to-1].Push(temp)
+		}
+	}
+}
