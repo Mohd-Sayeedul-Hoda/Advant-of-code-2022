f4979dab80d078c490700f02ed90a02053d0b6f0
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2023-12-22 15:06:42 +0530

    it take me time because of error that i encounter not naming directory "/this/that/" like this is was just putting first name only

---
+++ day07/go.mod
@@ -0,0 +1,3 @@
+module github.com/Mohd-Sayeedul-Hoda/day07
+
+go 1.20
---
+++ day07/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	part1 "github.com/Mohd-Sayeedul-Hoda/day07/part1"
+	//part2 "github.com/Mohd-Sayeedul-Hoda/day07/part2"
+)
+
+func main(){
+	part1.Part1();
+	//part2.Part2();
+}
